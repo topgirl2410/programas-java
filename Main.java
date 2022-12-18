6a116ff4fd99fd4dbbfadeab8dc7eb36075c555d
topgirl2410 <maria.topgirl82@gmail.com>
2022-12-18 15:43:49 +0100

    funciones en java

--- Main.java
+++ Main.java
@@ -1,13 +1,23 @@
 
 public class Main {
     public static void main(String[] args) {
-        int resultado = 0;
-        resultado = suma(20, 21, 30);
-        System.out.println(resultado);
-    }  
-    
+        Potato miPotato = new Potato();
+        miPotato.QuitarBrazo();
+        miPotato.QuitarBrazo();
+        miPotato.QuitarBrazo();
+        System.out.println(miPotato.brazos);
+    }
 
     public static int suma(int a, int b, int c) {
         return a + b + c;
     }
 }
+
+class Potato {
+    public int brazos = 4;
+
+    public void QuitarBrazo() {
+        this.brazos--;
+    }
+}
+
