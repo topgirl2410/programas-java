d89cdaba24105c78f21711b15c4c6426b03356c4
topgirl2410 <maria.topgirl82@gmail.com>
2022-12-18 15:32:00 +0100

    primer ejercicio

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int resultado = 0;
+        resultado = suma(20, 21, 30);
+        System.out.println(resultado);
+    }  
+    
+
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+}
